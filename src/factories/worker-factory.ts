5c1b5182f3d5817c27048f1df52a09e0238d438a
不知名の睡教高手 <i@mdzz.name>
2023-03-20 00:47:36 +0800

    chore: 查询代码优化 (#7)
    Co-authored-by: wenjunshu <wenjunshu123>

--- src/factories/worker-factory.ts
+++ src/factories/worker-factory.ts
@@ -1,7 +1,7 @@
 import { WebSocketServerAdapter } from '../adapters/web-socket-server-adapter.ts'
 import { AppWorker } from '../app/worker.ts'
 import { api, LocalBroker, WebSocketServerService } from '../core-services/index.ts'
-import { getMasterDbClient, getReadReplicaDbClient } from '../database/client.ts'
+import { getMasterDbClient, getReadReplicaDbClient } from '../database/client1.ts'
 import { createSettings } from '../factories/settings-factory.ts'
 import { EventRepository } from '../repositories/event-repository.ts'
 import { UserRepository } from '../repositories/user-repository.ts'
