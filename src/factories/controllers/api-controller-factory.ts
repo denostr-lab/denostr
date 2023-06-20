bbc9015db9a2495962b464224c4c07f0650b4fbd
唐琮霖 <huoer_15@163.com>
2023-06-21 00:53:10 +0800

    fix: voice list test

--- src/factories/controllers/api-controller-factory.ts
+++ src/factories/controllers/api-controller-factory.ts
@@ -1,5 +1,6 @@
 import { EventsController } from '@/controllers/api/events-controller.ts'
 import { InvoicesController } from '@/controllers/api/invoices-controller.ts'
+import { masterInvoicesModel } from '@/database/models/Invoices.ts'
 import { IController } from '@/@types/controllers.ts'
 
 export const createEventsController = (): IController => {
@@ -7,5 +8,5 @@ export const createEventsController = (): IController => {
 }
 
 export const createInvoicesController = (): IController => {
-    return new InvoicesController()
+    return new InvoicesController(masterInvoicesModel)
 }
