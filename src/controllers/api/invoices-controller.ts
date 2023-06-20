ef1aae30414660c31680bad5b6d668397c0837ac
唐琮霖 <huoer_15@163.com>
2023-06-21 00:53:10 +0800

    fix: voice list test

--- src/controllers/api/invoices-controller.ts
+++ src/controllers/api/invoices-controller.ts
@@ -1,11 +1,16 @@
 import { helpers, IController, Request, Response, RouterContext, Status } from '@/@types/controllers.ts'
-import { invoiceSchema, masterInvoicesModel } from '@/database/models/Invoices.ts'
+// import { invoiceSchema, masterInvoicesModel } from '@/database/models/Invoices.ts'
 import { Sort } from '@/constants/base.ts'
 import { path } from 'ramda'
 import { Filter } from 'mongodb'
-import { Schema } from 'mongoose'
+import mongoose, { Schema } from 'mongoose'
+import { DBInvoice } from '@/@types/invoice.ts'
 
 export class InvoicesController implements IController {
+    public constructor(
+        private readonly masterInvoicesModel: mongoose.PaginateModel<DBInvoice, {}, {}>,
+    ) {}
+
     public async handleRequest(_: Request, response: Response, ctx: RouterContext) {
         const query = helpers.getQuery(ctx)
         const { sortField = 'created_at', sortValue = 'desc' } = query
@@ -33,18 +38,18 @@ export class InvoicesController implements IController {
             }
         }
 
-        response.body = await masterInvoicesModel.paginate(filter, { sort, limit, page })
+        response.body = await this.masterInvoicesModel.paginate(filter, { sort, limit, page })
             .then((result) => ({
                 ...result,
                 docs: result.docs.map((doc) => {
                     return {
-                        _id: doc._doc?._id,
-                        pubkey: doc._doc?.pubkey,
-                        unit: doc._doc?.unit,
-                        status: doc._doc?.status,
-                        created_at: doc._doc?.created_at,
-                        amount_requested: Number(doc._doc?.amount_requested / 1000n),
-                        amount_paid: Number(doc._doc?.amount_paid / 1000n),
+                        _id: doc?._id,
+                        pubkey: doc?.pubkey,
+                        unit: doc?.unit,
+                        status: doc?.status,
+                        created_at: doc?.created_at,
+                        amount_requested: Number(doc?.amount_requested / 1000n),
+                        amount_paid: Number(doc?.amount_paid / 1000n),
                     }
                 }),
             }))
